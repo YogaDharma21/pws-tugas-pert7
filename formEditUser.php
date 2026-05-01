440a340b8691387a46d2eaa41b7395e0250b52d4
Yoga Dharma <yogacode2006@gmail.com>
2026-05-01 18:41:47 +0800

    fix(ui): update user and mahasiswa labels and titles
    Update the page title in the user edit form and correct the alert
    messages in the mahasiswa deletion process to use appropriate
    terminology.

--- formEditUser.php
+++ formEditUser.php
@@ -9,7 +9,7 @@ $data = $users->getById($_GET['id']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit User</title>
+    <title>Edit Data User</title>
 </head>
 
 <body>
--- process_delete_mahasiswa.php
+++ process_delete_mahasiswa.php
@@ -2,7 +2,7 @@
 require_once './classes/Mahasiswa.php';
 $mahasiswa = new Mahasiswa();
 if ($mahasiswa->delete($_GET['id'])) {
-    echo "<script> alert('User berhasil dihapus'); window.location.href = './formMahasiswa.php'; </script>";
+    echo "<script> alert('Mahasiswa berhasil dihapus'); window.location.href = './formMahasiswa.php'; </script>";
 } else {
-    echo "<script> alert('User gagal dihapus'); window.location.href = './formMahasiswa.php'; </script>";
+    echo "<script> alert('Mahasiswa gagal dihapus'); window.location.href = './formMahasiswa.php'; </script>";
 }
